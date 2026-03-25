fixed loss, added validation price calculation, set default run in main

src/RiskMeasures.py
@@ -94,24 +94,43 @@ class CVaR(torch.nn.Module):
     Output:
         1 x 1 (CVaR of portfolio values)
     '''
-    def __init__(self, p: float, beta: float = 1e-2, omega: float = 0.0):
+    def __init__(self, p: float, beta: float = 1e-2):
         super().__init__()
         self.p = p
         self.beta = beta
-        self.omega = nn.Parameter(torch.tensor(float(omega), dtype=torch.float32))
 
     def forward(self, portfolio_value: torch.Tensor):
         x = portfolio_value.view(-1)
         loss = -x
 
-        # VaR threshold
-        omega = self.omega.to(device=x.device, dtype=x.dtype)
+        # empirical VaR threshold on current batch, detached from graph
+        omega = loss.quantile(self.p).detach()
 
-        # smooth version of max(0, z)
+        # smooth approximation of (loss - omega)+
         excess = F.softplus((loss - omega) / self.beta) * self.beta
-        cvar_loss = omega + (1.0 / (1 - self.p)) * excess.mean()
+
+        cvar_loss = omega + (1.0 / (1.0 - self.p)) * excess.mean()
 
         return -cvar_loss
+    
+    # def __init__(self, p: float, beta: float = 1e-2, omega: float = 0.0):
+    #     super().__init__()
+    #     self.p = p
+    #     self.beta = beta
+    #     self.omega = nn.Parameter(torch.tensor(float(omega), dtype=torch.float32))
+
+    # def forward(self, portfolio_value: torch.Tensor):
+    #     x = portfolio_value.view(-1)
+    #     loss = -x
+
+    #     # VaR threshold
+    #     omega = self.omega.to(device=x.device, dtype=x.dtype)
+
+    #     # smooth version of max(0, z)
+    #     excess = F.softplus((loss - omega) / self.beta) * self.beta
+    #     cvar_loss = omega + (1.0 / (1 - self.p)) * excess.mean()
+
+    #     return -cvar_loss
 
 
 class Variance(RiskMeasure):
@@ -141,5 +160,4 @@ class ExpectationVariance(RiskMeasure):
         self.alpha = alpha
 
     def forward(self, portfolio_value: torch.Tensor):
-
-        return portfolio_value.mean() - self.alpha * portfolio_value.var()
+        return -(portfolio_value.mean() - self.alpha * portfolio_value.var())

src/agents/Agent.py
@@ -191,21 +191,30 @@ class Agent(torch.nn.Module, ABC):
             self.training_logs["training_losses"] = torch.Tensor(losses).cpu()
 
         return losses
+    
+
+    def get_price(self, contingent_claim: Claim, paths=int(1e5), T=55):
+        with torch.no_grad():
+            self.eval()
+            profit, _ = self.pl(contingent_claim, paths, T, False)
+            price = -self.criterion(profit).item()
+        return price
 
     def validate(self, contingent_claim: Claim, paths = int(1e6), T = 365, logging = True):
         """
         :param contingent_claim: Instrument
         :param epochs: int
         :param batch_size: int
-        :return: None
         """
         with torch.no_grad():
             self.eval()
             profit, claim_payoff = self.pl(contingent_claim, paths, T, True)
             loss = self.criterion(profit)
+            price = self.get_price(contingent_claim, T=T)
             if logging:
                 self.validation_logs["validation_profit"] = profit.detach().cpu()
                 self.validation_logs["validation_claim_payoff"] = claim_payoff.detach().cpu()
                 self.validation_logs["validation_loss"] = loss.detach().cpu()
+                self.validation_logs["price"] = price
 
             return loss.item()

src/agents/DeltaAgent.py
@@ -2,8 +2,8 @@ from typing import List
 import torch
 from ..Costs import CostFunction
 from ..agents.Agent import Agent
-from ..instruments.Instruments import Instrument
 from torch.distributions import Normal
+from ..instruments.Instruments import Instrument
 
 
 class DeltaAgent(Agent):
@@ -11,16 +11,16 @@ class DeltaAgent(Agent):
     Delta hedging agent that computes the delta of a European option and returns it as the action
     '''
     def __init__(self,
-                criterion: torch.nn.Module,
-                cost_function: CostFunction,
-                hedging_instruments: List[Instrument],
-                stock_params,
-                interest_rate = 0.0,
-                lr=0.005,
-                use_gpu=True,
-                h_dim=15,
-                ):
-        super().__init__(criterion, cost_function, hedging_instruments, interest_rate, lr, use_gpu)
+            criterion: torch.nn.Module,
+            cost_function: CostFunction,
+            hedging_instruments: List[Instrument],
+            stock_params,
+            interest_rate = 0.0,
+            lr=0.005,
+            use_gpu=True,
+            h_dim=15,
+            ):
+        super().__init__(criterion, None, cost_function, hedging_instruments, interest_rate, lr, use_gpu)
         self.strike, self.expiry, self.drift, self.volatility = stock_params
 
         #initialize deltas to zero
@@ -31,26 +31,28 @@ class DeltaAgent(Agent):
         days_to_expiry = torch.tensor(days_to_expiry)
         d1 = (torch.log(last_prices / self.strike) + (self.drift + 0.5 * self.volatility ** 2) * (days_to_expiry + eps)) / (self.volatility * torch.sqrt(days_to_expiry + eps))
         return Normal(0, 1).cdf(d1)
-
+    
 
     def feature_transform(self, state: tuple) -> torch.Tensor:
         """
         :param state: tuple
         :return: torch.Tensor
         """
-        # only know the current price
-        paths, cash_account, positions, T = state
+
+        paths, _, _, _ = state
         P, t, N = paths.shape
 
-        last_prices = paths[:, -1, :]
+        # сбрасываем если размер изменился
+        if self.current_delta.shape[0] != P:
+            self.current_delta = torch.zeros(P, N, device=self.device)
 
-        #get delta from last prices
+        last_prices = paths[:, -1, :]
         days_to_expiry = self.expiry - t
         delta = self.get_delta(last_prices, days_to_expiry)
         delta_diff = delta - self.current_delta
         self.current_delta = delta
-
         return delta_diff.to(self.device)
+    
 
     def forward(self, state: tuple) -> torch.Tensor:
         action = self.feature_transform(state) # D x input_dim

src/main.py
@@ -25,7 +25,7 @@ def main():
     seed = 133
     torch.manual_seed(seed)
 
-    T = 50
+    T = 55
     total_rate = 0.0
     step_interest_rate = (total_rate + 1) ** (1 / T) - 1
 
@@ -50,13 +50,12 @@ def main():
     contingent_claim: Claim = BSCall(stock, S0, T, drift, volatility=0.75)
     hedging_instruments: List[Instrument] = [stock]
 
-    epochs = 2500
+    epochs = 5000
     paths = int(1e5)
     verbose = True
 
-    # criterion = RiskMeasures.CVaR(0.95) # CVaR with confidence level 0.95
     criterion = RiskMeasures.CVaR(0.99) # CVaR with confidence level 0.99
-    # criterion2 = RiskMeasures.CVaR(0.5) # CVaR with confidence level 0.5
+    # criterion2 = RiskMeasures.CVaR(0.99) # CVaR with confidence level 0.5
 
     prop_cost = 0.01
     cost_function: CostFunction = ProportionalCost(prop_cost)
@@ -69,41 +68,64 @@ def main():
 
     # # Delta hedging agent - requires strike, expiry, drift, volatility as stock_params
     # delta_params = (S0, T, drift, 0.75)  # strike, expiry, drift, volatility
-    # delta_runner = SimpleRunner("delta", use_gpu=True)
+    # delta_runner = SimpleRunner("Delta", use_gpu=True)
     # res = delta_runner.run(contingent_claim, hedging_instruments, criterion, adam_optimizer, T, step_interest_rate, epochs, paths, verbose, cost_function, stock_params=delta_params)
     # print(res)
     # runners.append(delta_runner)
 
     # # Simple agent - trainable, no stock_params needed
-    # simple_runner = SimpleRunner("simple", use_gpu=True)
-    # h_dim = 15
+    # simple_runner = SimpleRunner("Simple", use_gpu=True)
+    # h_dim = 16
     # res = simple_runner.run(contingent_claim, hedging_instruments, criterion, adam_optimizer, T, step_interest_rate, epochs, paths, verbose, cost_function, h_dim)
     # print(res)
     # runners.append(simple_runner)
 
+    # # Recurrent agent - trainable, no stock_params needed
+    # recurrent_runner_adam = SimpleRunner("Recurrent", use_gpu=True)
+    # h_dim = 16
+    # res = recurrent_runner_adam.run(contingent_claim, hedging_instruments, criterion, adam_optimizer, T, step_interest_rate, epochs, paths, verbose, cost_function, h_dim)
+    # print(res)
+    # runners.append(recurrent_runner_adam)
+
+    # # Recurrent agent - trainable, no stock_params needed
+    # recurrent_runner_adam = SimpleRunner("CVaR (p = 0.5)", use_gpu=True)
+    # h_dim = 16
+    # res = recurrent_runner_adam.run(contingent_claim, hedging_instruments, criterion, adam_optimizer, T, step_interest_rate, epochs, paths, verbose, cost_function, h_dim)
+    # print(res)
+    # runners.append(recurrent_runner_adam)
+
+
+    # # Recurrent agent - trainable, no stock_params needed
+    # recurrent_runner_adam = SimpleRunner("CVaR (p = 0.99)", use_gpu=True)
+    # h_dim = 16
+    # res = recurrent_runner_adam.run(contingent_claim, hedging_instruments, criterion2, adam_optimizer, T, step_interest_rate, epochs, paths, verbose, cost_function, h_dim)
+    # print(res)
+    # runners.append(recurrent_runner_adam)
+
+
     # Recurrent agent - trainable, no stock_params needed
-    recurrent_runner_sgld = SimpleRunner("recurrent_sgld", use_gpu=True)
-    h_dim = 32
+    recurrent_runner_adam = SimpleRunner("Adam", use_gpu=True)
+    h_dim = 1
+    res = recurrent_runner_adam.run(contingent_claim, hedging_instruments, criterion, adam_optimizer, T, step_interest_rate, epochs, paths, verbose, cost_function, h_dim)
+    print(res)
+    runners.append(recurrent_runner_adam)
+
+    # Recurrent agent - trainable, no stock_params needed
+    recurrent_runner_sgld = SimpleRunner("SGLD", use_gpu=True)
+    h_dim = 16
     res = recurrent_runner_sgld.run(contingent_claim, hedging_instruments, criterion, sgld_optimizer, T, step_interest_rate, epochs, paths, verbose, cost_function, h_dim)
     print(res)
     runners.append(recurrent_runner_sgld)
 
     # Recurrent agent - trainable, no stock_params needed
-    recurrent_runner_sghmc = SimpleRunner("recurrent_sghmc", use_gpu=True)
-    h_dim = 32
+    recurrent_runner_sghmc = SimpleRunner("SGHMC", use_gpu=True)
+    h_dim = 16
     res = recurrent_runner_sghmc.run(contingent_claim, hedging_instruments, criterion, sghmc_optimizer, T, step_interest_rate, epochs, paths, verbose, cost_function, h_dim)
     print(res)
     runners.append(recurrent_runner_sghmc)
 
-    # Recurrent agent - trainable, no stock_params needed
-    recurrent_runner_adam = SimpleRunner("recurrent_adam", use_gpu=True)
-    h_dim = 32
-    res = recurrent_runner_adam.run(contingent_claim, hedging_instruments, criterion, adam_optimizer, T, step_interest_rate, epochs, paths, verbose, cost_function, h_dim)
-    print(res)
-    runners.append(recurrent_runner_adam)
 
-
-    plot_dists(runners, save=True, file_prefix=f"outputs/new_recurrent_sgld_sghmc_adam_cvar0_99_0_01_cost")
+    plot_dists(runners, save=True, file_prefix=f"outputs/adam_sgld_sghmc_0_99")
     plt.show()
 
 

src/optimizers.py
@@ -60,7 +60,7 @@ class SGHMCOptimizer(torch.optim.Optimizer):
     '''
     Stochastic Gradient Hamiltonian Monte Carlo
     '''
-    def __init__(self, params, lr=0.005, weight_decay=0.0, momentum=0.9, temperature=0.00005):
+    def __init__(self, params, lr=0.005, weight_decay=0.0, momentum=0.9, temperature=0.000001):
         defaults = dict(lr=lr, weight_decay=weight_decay, momentum=momentum, temperature=temperature)
         super().__init__(params, defaults)
         for group in self.param_groups: